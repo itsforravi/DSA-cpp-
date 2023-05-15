9927279e71a160518adb17cf6285a78ce14f937b
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-05-15 11:54:50 +0530

    DSA

--- First.cpp
+++ First.cpp
@@ -2,9 +2,13 @@
 typedef unsigned long long int ll;
 using namespace std;
 
-void printName(string name){
-    cout<<"Hay "<<name;
+int max(int num1,int num2){
+  if(num1>=num2){
+    return num1;
+  }
+  else return num2;
 }
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -12,9 +16,13 @@ int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
-    string name;
-    cin>>name;
-printName(name);
+   int num1,num2;
+   cin>>num1>>num2;
+   int minimum=max(num1,num2);
+
+
+   cout<< minimum;
+
  return 0;
 
 }
