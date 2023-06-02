77e1c7ebe9b8bdad41cdffd4e620f9189d17ee2c
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-02 13:48:44 +0530

    dsa

--- BASICS/Recursion/recursion.cpp
+++ BASICS/Recursion/recursion.cpp
@@ -261,7 +261,18 @@ void Selection_Sort(int arr[], int n) {
     arr[mini] = arr[i];
     arr[i] = temp;
   }}
-
+//Buble Sort
+void bubble_sort(int arr[], int n){
+  for (int i = n-1 ; i >= 0; i-- ){
+     for ( int j = 0 ; j<= i-1 ; j++){
+      if (arr[j] > arr[j+1]){
+        int temp = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = temp;
+      }
+     }
+  }
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -386,14 +397,15 @@ int main(){
 
 // Selection_Sort
 
- int arr[] = {13,46,24,52,20,9};
+ int arr[] = {13,46,24,52,20,3};
   int n = sizeof(arr) / sizeof(arr[0]);
  cout << "Before selection sort: " << "\n";
    for (int i = 0; i < n; i++) {
     cout << arr[i] << " ";
   }
   cout << endl;
-  Selection_Sort(arr, n);
+  // Selection_Sort(arr, n);
+  bubble_sort(arr,n);
     cout << "After selection sort: " << endl;
   for (int i = 0; i < n; i++) {
     cout << arr[i] << " ";
