dab4f88e97e9361cfb7050b42a5b530c86965ecf
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-02 18:21:53 +0530

    dsa

--- BASICS/Array/Easy/largeElement.cpp
+++ BASICS/Array/Easy/largeElement.cpp
@@ -2,6 +2,18 @@
 typedef unsigned long long int ll;
 using namespace std;
 
+// Largest Element 
+int largestElement(vector<int> &arr, int n) {
+   int largest=arr[0];
+   for(int i=0; i<n; i++){
+      while(arr[i] > largest){
+         largest=arr[i];
+      }
+   }
+   return largest;
+}
+
+
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -10,5 +22,11 @@ int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
+    // Largest Element 
+    vector<int> arr={3,2,4,52,67,17};
+    int n=6;
+    int max=largestElement(arr,n);
+    cout<<"Largest element in the arr " <<max<<" ";
+
     return 0;
 }
