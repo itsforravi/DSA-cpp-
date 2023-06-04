d371ad13e22984dcc83170bc4585b724535a46d0
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-04 13:58:16 +0530

    dsa

--- BASICS/Array/Easy/largeElement.cpp
+++ BASICS/Array/Easy/largeElement.cpp
@@ -81,18 +81,29 @@ void leftRotation(int arr[],int n){
 }
 
 // Rotate array by K elements
+// {Brute Force}
+// void leftd(int arr[], int n, int d){
+//     d=d%n;
+//     int temp[d];
+//     for(int i=0;i<d;i++){
+//         temp[i]=arr[i];
+//     }
+//     for(int i=d;i<n;i++){
+//         arr[i-d]=arr[i];
+//     }
+//     for(int i=n-d;i<n;i++){
+//         arr[i]=temp[i-(n-d)];
+//     }
+// }
+
+// {Optimal Solution}
 void leftd(int arr[], int n, int d){
-    d=d%n;
-    int temp[d];
-    for(int i=0;i<d;i++){
-        temp[i]=arr[i];
-    }
-    for(int i=d;i<n;i++){
-        arr[i-d]=arr[i];
-    }
-    for(int i=n-d;i<n;i++){
-        arr[i]=temp[i-(n-d)];
-    }
+  if(n==0) return;
+  d=d%n;
+  reverse(arr,arr+d);
+  reverse(arr+d,arr+n);
+  reverse(arr,arr+n);
+
 }
 
 int main(){
@@ -146,7 +157,15 @@ int main(){
 int n=7;
 int arr[]={3,4,2,5,6,1,7};
 int d=3;
+
+cout<<"Before Left Rotation " << endl;
+for(int i=0;i<n;i++){
+    cout<<arr[i]<< " ";
+}
+cout<<endl;
+
   leftd(arr,n,d);
+  cout<<"Before Left Rotation " << endl;
   for(int i=0;i<n;i++){
       cout<<arr[i]<<" ";
   }
