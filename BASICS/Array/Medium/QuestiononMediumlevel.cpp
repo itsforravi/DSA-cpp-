e78a76161aaef2ad8a269e1ba22572aab348a3ec
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-25 17:32:33 +0530

    dsa

--- BASICS/Array/Medium/QuestiononMediumlevel.cpp
+++ BASICS/Array/Medium/QuestiononMediumlevel.cpp
@@ -290,26 +290,66 @@ return longest;
 }
 
 // Set matrix zero
+// Better Solution
 vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
-int row[n]={0};
-int col[m]={0};
+// int row[n]={0};
+// int col[m]={0};
+// for(int i=0;i<n;i++){
+// 	for(int j=0;j<m;j++){
+// 		if(matrix[i][j]==0){
+// 			row[i]=1;
+// 			col[j]=1;
+// 		}
+// 	}
+// }	
+
+
+// for(int i=0;i<n;i++){
+//   for (int j = 0; j < m; j++) {
+//     if (row[i] || col[j]) {
+//       matrix[i][j] = 0;
+//     }
+//           }
+//   }
+
+// Best Solution
+int col0=1;
+
 for(int i=0;i<n;i++){
 	for(int j=0;j<m;j++){
-		if(matrix[i][j]==0){
-			row[i]=1;
-			col[j]=1;
-		}
-	}
-}	
+          if (matrix[i][j] == 0) {
+			  matrix[i][0]=0;
+			  if(j !=0)
+			  matrix[0][j]=0;
+			  else
+			  col0=0;
+          }
+        }
+}
 
 
 for(int i=0;i<n;i++){
   for (int j = 0; j < m; j++) {
-    if (row[i] || col[j]) {
-      matrix[i][j] = 0;
-    }
-          }
+ if(matrix[i][j] !=0){
+	 if(matrix[0][j]==0 || matrix[i][0]==0){
+		 matrix[i][j]=0;
+	 }
+ }
+ 
   }
+}
+
+if(matrix[0][0]==0){
+  for (int j = 0; j < m; j++) {
+    matrix[0][j] = 0;
+  }
+}
+
+if(col0==0){
+	for(int i=0;i<n;i++){
+		matrix[i][0]=0;
+	}
+}
 return matrix;
 } 
 
@@ -408,9 +448,9 @@ int main(){
 
 
 // // Set matrix zero
-vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+vector<vector<int>> matrix = {{2,3}, {2,4,3}, {1, 0,0}};
     int n = matrix.size();
-    int m = matrix[0].size();
+    int m = matrix.size();
     vector<vector<int>> ans = zeroMatrix(matrix, n, m);
 
     cout << "The Final matrix is: n";
