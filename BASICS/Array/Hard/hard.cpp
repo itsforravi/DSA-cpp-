5de04b8520cb17bd32574e4014df34416504cd9c
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-26 12:30:14 +0530

    dsa

--- BASICS/Array/Hard/hard.cpp
+++ BASICS/Array/Hard/hard.cpp
@@ -20,6 +20,26 @@ int pascalTriangle(int r ,int c){
 }
 
 
+// Type : 2
+
+int nCr1(int n,int r){
+    long long res=1;
+    for(int i=0;i<r;i++){
+        res=res*(n-i);
+        res=res/(i+1);
+
+    }
+    return res;
+}
+
+void pascalTriangle1(int n){
+    for(int c=1;c<=n; c++){
+        cout<<nCr1(n-1,c-1)<<" ";
+    }
+    cout<<" ";
+}
+
+
 
 
 
@@ -31,11 +51,17 @@ int main(){
     freopen("output.txt","w",stdout);
     #endif
 
+    // Type :1
+    // int r=5;
+    // int c=3;
+    // int element =pascalTriangle(r,c);
+    // cout<<"Pascal Triangle (r,c) is => " << element<< " ";
+    
 
-    int r=5;
-    int c=3;
-    int element =pascalTriangle(r,c);
-    cout<<"Pascal Triangle (r,c) is => " << element<< " ";
+    // Type :2
+    int n=6;
+    cout<<"Pascal Tringle row number "<<n<<endl;
+    pascalTriangle1(n);
     
 
 
