5ecc0e189205d314d86f13d5765fb3ec74ec7321
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-26 16:43:49 +0530

    dsa

--- BASICS/Array/Hard/hard.cpp
+++ BASICS/Array/Hard/hard.cpp
@@ -69,22 +69,57 @@ vector<vector<int>> pascalTriangle2(int n) {
 
 // Majority Elements(>N/3 times) 
 vector<int> majorityElement(vector<int> v) {
-	vector<int> ls;
-	map<int,int> mpp;
-	int n=v.size();
-	int mini=(int)(n/3)+1;
-	for(int i=0;i<n;i++){
-		mpp[v[i]]++;
-		if(mpp[v[i]]==mini){
-			ls.push_back(v[i]);
-		}
-		if(ls.size()==2) break;
+	// Better Solution
+    
+    // vector<int> ls;
+	// map<int,int> mpp;
+	// int n=v.size();
+	// int mini=(int)(n/3)+1;
+	// for(int i=0;i<n;i++){
+	// 	mpp[v[i]]++;
+	// 	if(mpp[v[i]]==mini){
+	// 		ls.push_back(v[i]);
+	// 	}
+	// 	if(ls.size()==2) break;
+	// }
+	// sort(ls.begin(),ls.end());
+	// return ls;
+
+    //Optimal solution
+    int cnt1=0,cnt2=0;
+int el1=INT_MIN;
+int el2=INT_MIN;
+for(int i=0;i<v.size();i++){
+	if(cnt1==0 && el2!=v[i]){
+		cnt1=1;
+		el1=v[i];
 	}
-	sort(ls.begin(),ls.end());
-	return ls;
-
-
+	else if (cnt2==0 && el1!=v[i]){
+		cnt2=1;
+		el2=v[i];
+	}
+	else if (el1==v[i]) cnt1++;
+	else if (el2==v[i]) cnt2++;
+	else{
+		cnt1--;
+		cnt2--;
+	}
 }
+vector<int> ls;
+cnt1=0,cnt2=0;
+for(int i=0;i<v.size();i++){
+	if(el1==v[i]) cnt1++;
+	if(el2==v[i]) cnt2++;
+}
+int mini=(int)(v.size()/3)+1;
+if(cnt1>=mini) ls.push_back(el1);
+if(cnt2>=mini) ls.push_back(el2);
+sort(ls.begin(),ls.end());
+return ls;
+}
+
+
+
 
 
 
@@ -125,7 +160,7 @@ int main(){
 
 
 // Majority Elements(>N/3 times) 
-vector<int> arr={11,33,22,33,22,11,11};
+vector<int> arr={11,22,33,22,33,22,11,11};
 vector<int> element=majorityElement(arr);
  cout << "The majority elements are: "<<endl;
 
