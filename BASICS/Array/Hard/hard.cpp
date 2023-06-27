3c87184348ba47c958b404721464b434b7306e4f
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-27 18:01:45 +0530

    dsa

--- BASICS/Array/Hard/hard.cpp
+++ BASICS/Array/Hard/hard.cpp
@@ -338,22 +338,22 @@ vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr){
 
 
 // Optimal Solution
-// vector<vector<int>> mergeOverlappingIntervals1(vector<vector<int>> &arr){
-// 	int n=arr.size();
-// 	sort(arr.begin(),arr.end());
-// 	vector<vector<int>> ans;
-// 	for(int i=0;i<n;i++){
-// 	if(ans.empty()|| arr[i][0] >ans.back()[1]){
-//        ans.push_back(arr[i]);
-// 	}
-// 	else{
-// 		ans.back()[1] =max(ans.back()[1],arr[i][1]);
-// 	}
-// 	} 
-// 	return ans;
+vector<vector<int>> mergeOverlappingIntervals1(vector<vector<int>> &arr){
+	int n=arr.size();
+	sort(arr.begin(),arr.end());
+	vector<vector<int>> ans;
+	for(int i=0;i<n;i++){
+	if(ans.empty()|| arr[i][0] >ans.back()[1]){
+       ans.push_back(arr[i]);
+	}
+	else{
+		ans.back()[1] =max(ans.back()[1],arr[i][1]);
+	}
+	} 
+	return ans;
 
 	
-// }
+}
 
 
 int main(){
@@ -465,8 +465,8 @@ int main(){
 
 
 // Merge Overlapping Sub-intervals
- vector<vector<int>> arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
-    vector<vector<int>> ans = mergeOverlappingIntervals(arr);
+ vector<vector<int>> arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18},{19,22}};
+    vector<vector<int>> ans = mergeOverlappingIntervals1(arr);
     cout << "The merged intervals are: " << "\n";
     for (auto it : ans) {
         cout << "[" << it[0] << ", " << it[1] << "] ";
