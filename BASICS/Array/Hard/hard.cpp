0020072b8d608b20dd43972d7030b028b68f9cfc
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-27 18:00:32 +0530

    dsa

--- BASICS/Array/Hard/hard.cpp
+++ BASICS/Array/Hard/hard.cpp
@@ -310,6 +310,50 @@ int subarraysWithSumK3(vector < int > a, int b) {
 }
 
 
+// Merge Overlapping Sub-intervals
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr){
+	int n=arr.size();
+	sort(arr.begin(),arr.end());
+	vector<vector<int>> ans;
+	for(int i=0;i<n;i++){
+		int start=arr[i][0];
+		int end=arr[i][1];
+		if(!ans.empty() && end <= ans.back()[1]){
+			continue;
+		}
+		for(int j=i+1;j<n;j++){
+			if(arr[j][0]<=end){
+				end=max(end,arr[j][1]);
+			}
+			else{
+				break;
+			}
+		}
+		ans.push_back({start,end});
+	}
+	return ans;
+
+	
+}
+
+
+// Optimal Solution
+// vector<vector<int>> mergeOverlappingIntervals1(vector<vector<int>> &arr){
+// 	int n=arr.size();
+// 	sort(arr.begin(),arr.end());
+// 	vector<vector<int>> ans;
+// 	for(int i=0;i<n;i++){
+// 	if(ans.empty()|| arr[i][0] >ans.back()[1]){
+//        ans.push_back(arr[i]);
+// 	}
+// 	else{
+// 		ans.back()[1] =max(ans.back()[1],arr[i][1]);
+// 	}
+// 	} 
+// 	return ans;
+
+	
+// }
 
 
 int main(){
@@ -413,12 +457,20 @@ int main(){
 //     cout << solve(a) << endl;
 
 // Count the number of subarrays with given xor K
-vector<int> a={4, 2,3,2, 6, 4};
-int b;
-cin>>b;
-int ans=subarraysWithSumK3(a,b);
-cout<<" The number of subarrays with XOR k is: "<<ans; 
+// vector<int> a={4, 2,3,2, 6, 4};
+// int b;
+// cin>>b;
+// int ans=subarraysWithSumK3(a,b);
+// cout<<" The number of subarrays with XOR k is: "<<ans; 
 
 
+// Merge Overlapping Sub-intervals
+ vector<vector<int>> arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+    vector<vector<int>> ans = mergeOverlappingIntervals(arr);
+    cout << "The merged intervals are: " << "\n";
+    for (auto it : ans) {
+        cout << "[" << it[0] << ", " << it[1] << "] ";
+    }
+    cout << endl;
     return 0;
 }
