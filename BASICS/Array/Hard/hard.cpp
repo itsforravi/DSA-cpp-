dde72c75038dd4e3ae3204fec82ed91f06476bce
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-28 17:36:42 +0530

    dsa

--- BASICS/Array/Hard/hard.cpp
+++ BASICS/Array/Hard/hard.cpp
@@ -474,6 +474,55 @@ long long y = (x - val1);
 return {(int)x, (int)y};
 }
 
+// Optimal solution using XOR
+vector<int> findMissingRepeatingNumbers2(vector < int > a) {
+long long n=a.size();
+int xr=0;
+for (int i = 0; i < n; i++) {
+  xr = xr ^ a[i];
+  xr = xr ^ (i + 1);
+}
+    // int bitNo=0;
+    // while (1) {
+    //     if((xr &(1<<bitNo))!=0){
+    //         break;
+    //     }
+    //     bitNo++;
+    // }
+
+    int number=xr&(~(xr-1));
+    int zero=0;
+    int one=0;
+    for(int  i=0;i<n;i++){
+        if((a[i] & number)!=0){
+            one=one^a[i];
+
+        }
+        else{
+            zero=zero^a[i];
+
+        }
+    }
+    for(int i=1;i<=n;i++){
+          if((i & number)!=0){
+            one=one^i;
+
+        }
+        else{
+            zero=zero^i;
+
+        }
+
+    }
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        if(a[i]==zero) cnt++;
+    }
+    if(cnt==2) return {zero,one};
+    return {one,zero};
+}
+
+
 
 
 int main(){
@@ -642,7 +691,7 @@ int main(){
 // cout<<" ]";
 
 // Find the repeating and missing numbers
- vector<int> a = {3, 1, 2, 5, 4, 6, 8, 4};
+ vector<int> a = {3, 1, 2, 7, 4, 6, 8, 4};
     vector<int> ans = findMissingRepeatingNumbers1(a);
     cout << "The repeating and missing numbers are: {"
          <<"Repeating Number =>"<< ans[0] << ", " <<"Missing Nuber=> "<< ans[1] << "}\n";
