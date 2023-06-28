abe75e2292371d8a18a67816c68f58d7705f1673
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-28 19:22:42 +0530

    dsa

--- BASICS/Array/Hard/hard.cpp
+++ BASICS/Array/Hard/hard.cpp
@@ -533,7 +533,61 @@ int Countinversion(vector<int> &a,int n){
     }
     return cnt;
 }
+// Optimal Solution
 
+int merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //storing elements in the temporary array in a sorted manner//
+      int cnt=0;
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            cnt+=(mid-left+1);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+    return cnt;
+}
+
+int mergeSort(vector<int> &arr, int low, int high) {
+    int cnt=0;
+    if (low >= high) return cnt;
+    int mid = (low + high) / 2 ;
+    cnt+=mergeSort(arr, low, mid);  // left half
+    cnt+=mergeSort(arr, mid + 1, high); // right half
+   cnt+= merge(arr, low, mid, high);  // merging sorted halves
+   return cnt;
+}
+
+int numberOfInversions(vector<int>&a, int n) {
+  return  mergeSort(a,0,n-1);
+ 
+}
 
 
 int main(){
@@ -708,10 +762,20 @@ int main(){
 //          <<"Repeating Number =>"<< ans[0] << ", " <<"Missing Nuber=> "<< ans[1] << "}\n";
 
 // Count inversions in an array
-vector<int> a={5,4,3,2,1};
+// vector<int> a={5,4,3,2,1};
+// int n;
+// cin>>n;
+// int ans=Countinversion(a,n);
+// cout<<"Cout of invers in an array => "<<ans<<endl;
+
+// Opitmal Solution
+vector<int> a={6,5,4,3,2,1};
 int n;
 cin>>n;
-int ans=Countinversion(a,n);
+int ans=numberOfInversions(a,n);
 cout<<"Cout of invers in an array => "<<ans<<endl;
+
+
+
     return 0;
 }
