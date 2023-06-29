fe4fc1c6f033782196b84e5f571db6a0d45de21b
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-29 11:42:50 +0530

    dsa

--- BASICS/Array/Hard/hard.cpp
+++ BASICS/Array/Hard/hard.cpp
@@ -591,6 +591,7 @@ int numberOfInversions(vector<int>&a, int n) {
 
 
 // Count Reverse Pairs
+// Brute force
 int count(vector<int> &a,int n){
     int cnt=0;
     for(int i=0;i<n;i++){
@@ -605,6 +606,72 @@ int count(vector<int> &a,int n){
 int team(vector <int> & skill, int n)
 {
    return  count(skill,n);
+}
+
+// Optimal solution
+void merge1(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+int countPairs(vector<int> &arr,int low,int mid,int high){
+    int right=mid+1;
+    int cnt=0;
+    for(int i=low;i<=mid;i++){
+        while(right <= high && arr[i]>2*arr[right]) right++;
+        cnt+=(right-(mid+1));
+
+    }
+    return cnt;
+}
+int mergeSort1(vector<int> &arr, int low, int high) {
+    int cnt=0;
+    if (low >= high) return cnt;
+    int mid = (low + high) / 2 ;
+   cnt+= mergeSort1(arr, low, mid);  // left half
+  cnt+=  mergeSort1(arr, mid + 1, high); // right half
+   cnt+= countPairs(arr,low,mid,high);
+    merge1(arr, low, mid, high);  // merging sorted halves
+    return cnt++;
+}
+
+
+int team1(vector <int> & skill, int n)
+{
+  
+   return   mergeSort1(skill,0,n-1);
 }
 
 
@@ -795,10 +862,10 @@ int main(){
 // cout<<"Cout of invers in an array => "<<ans<<endl;
 
 // // Count Reverse Pairs
-vector<int> a={4,1,2,3,1};
+vector<int> a={5,4,1,2,3,1};
 int n;
 cin>>n;
-int ans=team(a,n);
+int ans=team1(a,n);
 cout<<"Cout of invers in an array => "<<ans<<endl;
 
 
