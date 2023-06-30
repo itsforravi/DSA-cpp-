94336997f7f6ac9fb11ff02bfd3c7f6e2c1b65e4
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-30 11:14:59 +0530

    dsa

--- BASICS/4.Binary Search/BS on 1D Arrays/1DArray.cpp
+++ BASICS/4.Binary Search/BS on 1D Arrays/1DArray.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 typedef unsigned long long int ll;
 using namespace std;
+
+// Binary Search to find in X sorted Array
+// Iterative Implementation
 int search(vector<int>& nums, int target) {
     int n=nums.size();
     int low=0,high=n-1;
@@ -13,6 +16,22 @@ int search(vector<int>& nums, int target) {
     return -1;
 }
 
+// Recursive  Implementation
+int bs(vector<int> &arr,int low,int high,int target){
+    if(low>high) return -1;
+    int mid=(low+high)/2;
+    if(arr[mid]==target) return mid;
+    else if(target>arr[mid]) return bs(arr,mid+1,high,target);
+    return bs(arr,low,mid-1,target);
+}
+
+int search1(vector<int>& nums, int target) {
+    
+    return bs(nums,0,nums.size()-1,target);
+
+
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -20,9 +39,19 @@ int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
+    // Binary Search to find in X sorted Array
+// Iterative Implementation
+    // vector<int> arr={3,4,5,6,7,8,9,10,11,12};
+    // int target;
+    // cin>>target;
+    // int ans=search(arr,target);
+    // cout<<"Search in the array is => "<<ans<<" ";
+
+
+// Recursive  Implementation
     vector<int> arr={3,4,5,6,7,8,9,10,11,12};
     int target;
     cin>>target;
-    int ans=search(arr,target);
+    int ans=search1(arr,target);
     cout<<"Search in the array is => "<<ans<<" ";
 }
