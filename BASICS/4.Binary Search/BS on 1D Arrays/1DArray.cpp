66c5e1a31a04e67701306474203e9e0a8cc0cbdc
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-30 14:12:37 +0530

    dsa

--- BASICS/4.Binary Search/BS on 1D Arrays/1DArray.cpp
+++ BASICS/4.Binary Search/BS on 1D Arrays/1DArray.cpp
@@ -32,6 +32,26 @@ int search1(vector<int>& nums, int target) {
 
 }
 
+// Lower Bound
+int lowerBound(vector<int> arr, int n, int x) {
+	int low=0,high=n-1;
+	int ans=n;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]>= x) {
+			ans=mid;
+			high=mid-1;
+		}
+		
+		else{
+			low=mid+1;
+		} 
+
+	}
+	return ans;
+}
+
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -49,9 +69,17 @@ int main(){
 
 
 // Recursive  Implementation
-    vector<int> arr={3,4,5,6,7,8,9,10,11,12};
+    // vector<int> arr={3,4,5,6,7,8,9,10,11,12};
+    // int target;
+    // cin>>target;
+    // int ans=search1(arr,target);
+    // cout<<"Search in the array is => "<<ans<<" ";
+
+    // Lower bound
+    vector<int> arr={3, 5, 8, 15, 19};
     int target;
     cin>>target;
-    int ans=search1(arr,target);
-    cout<<"Search in the array is => "<<ans<<" ";
+    int n=5;
+    int ans=lowerBound(arr,n,target);
+    cout<<"Lower Bound Search in the array is => "<<ans<<" ";
 }
