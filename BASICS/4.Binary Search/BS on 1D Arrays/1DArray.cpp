ab4074a74a283c1de768c229f55a9cbe4c9ace84
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-06-30 17:34:07 +0530

    dsa

--- BASICS/4.Binary Search/BS on 1D Arrays/1DArray.cpp
+++ BASICS/4.Binary Search/BS on 1D Arrays/1DArray.cpp
@@ -90,6 +90,50 @@ int upperBound(vector<int> &arr, int x, int n){
 	return ans;
 }
 
+// Floor and Ceil in Sorted Array
+int floor(int arr[],int n,int x){
+	int low=0,high=n-1;
+	int ans=-1;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]<=x){
+			ans=arr[mid];
+			low=mid+1;
+		}
+		else{
+			high=mid-1;
+		}
+	}
+	return ans;
+
+}
+
+int ceil(int arr[],int n,int x){
+	int low=0,high=n-1;
+	int ans=-1;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]>=x){
+			ans=arr[mid];
+			high=mid-1;
+		}
+		else{
+			low=mid+1;
+			
+		}
+	}
+	return ans;
+
+}
+
+pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
+	int f=floor(arr,n,x);
+	int c=ceil(arr,n,x);
+	return make_pair(f,c);
+	
+}
+
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -140,4 +184,11 @@ int main(){
 //     cout<<" Search insert  in the array is => "<<ans<<" ";
 
 
+// Floor and Ceil in Sorted Array
+int arr[] = {3, 4, 4, 7, 8, 10};
+	int n = 6, x = 9;
+	pair<int, int> ans = getFloorAndCeil(arr, n, x);
+	cout << "The floor and ceil are: " << ans.first
+	     << " " << ans.second << endl;
+         return 0;
 }
