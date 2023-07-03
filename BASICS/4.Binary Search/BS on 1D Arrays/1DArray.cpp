f92333015e4bc529f516198eddbcd47eeb55e4f1
Ravi Kumar Gupt <itsforravi@gmail.com>
2023-07-03 19:44:46 +0530

    dsa

--- BASICS/4.Binary Search/BS on 1D Arrays/1DArray.cpp
+++ BASICS/4.Binary Search/BS on 1D Arrays/1DArray.cpp
@@ -471,6 +471,33 @@ int floorSqrt(int n)
     return high;
 }
 
+// Nth Root of a Number using Binary Search
+int func(int mid,int n,int m){
+  long long ans=1;
+  for(int i=1;i<=n;i++){
+    ans=ans*mid;
+    if(ans>m) return 2;
+  }
+  if(ans==m) return 1;
+  return 0;
+}
+
+int  NthRoot(int n, int m) {
+  long long low=1,high=m;
+  while(low<=high){
+    long long mid=(low+high)/2;
+    long long  midN=func(mid,n,m);
+    if(midN==1) {
+      return mid;
+    } else if (midN == 0) {
+      low + mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
+
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -608,10 +635,16 @@ int main(){
 
 
 // // Find square root of a number in log…
-int n;
-cin>>n;
-int ans=floorSqrt(n);
-cout<<"Square root of a number: "<<ans<<" ";
+// int n;
+// cin>>n;
+// int ans=floorSqrt(n);
+// cout<<"Square root of a number: "<<ans<<" ";
+
+// Nth Root of a Number using Binary Search
+int n=4,m=27;
+
+int ans=NthRoot(n,m);
+cout<<"Nth Root of a Number using Binary Search: "<<ans<<" ";
 
 return 0;
 
