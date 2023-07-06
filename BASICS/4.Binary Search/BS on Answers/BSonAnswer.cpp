ceca79830e4dfe89039bcf9bfe3efc03e99df0ce
Ravi Kumar Gupt <87171097+itsforravi@users.noreply.github.com>
2023-07-06 16:48:08 +0530

    dsa

--- BASICS/4.Binary Search/BS on Answers/BSonAnswer.cpp
+++ BASICS/4.Binary Search/BS on Answers/BSonAnswer.cpp
@@ -355,6 +355,8 @@ int splitArray(vector<int>& nums, int k) {
 //     return 0.0;
 //     }
     
+
+    
 float median(int nums1[],int nums2[],int m,int n) {
     if(m>n)
         return median(nums2,nums1,n,m);
@@ -378,7 +380,39 @@ float median(int nums1[],int nums2[],int m,int n) {
         else low = cut1+1;
     }
     return 0.0;
-}
+    
+    }
+
+
+// K-th Element of two sorted arrays
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        if(n>m ){
+            return kthElement(arr1,arr2,n,m,k);
+            
+        }
+        int low=max(0,k-m),high=min(k,n);
+        while(low<=high){
+            int cut1=(low+high)>>1;
+            int cut2=k-cut1;
+            int l1=cut1==0 ? INT_MIN:arr1[cut1-1]; 
+            int l2=cut2==0 ? INT_MIN:arr2[cut2-1]; 
+            int r1=cut1==n ? INT_MAX:arr1[cut1]; 
+            int r2=cut2==m ? INT_MAX:arr2[cut2]; 
+            if(l1<=r2 && l2<=r1){
+                return max(l1,l2);
+            }
+            else if(l1>l2){
+                high=cut1-1;
+            }
+            else{
+                low=cut1+1;
+            }
+        }
+        
+    
+    return 1;
+    }
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -489,13 +523,23 @@ int main(){
 
 // // Median of Two Sorted Arrays of different sizes
 
+// int nums1[] = {1,4,7,10,12};
+//     int nums2[] = {2,3,6,15};
+//     int m = sizeof(nums1)/sizeof(nums1[0]);
+//     int n = sizeof(nums2)/sizeof(nums2[0]);
+//     cout<<"The Median of two sorted arrays is "<<fixed<<setprecision(5)
+//     <<median(nums1,nums2,m,n);
+
+// K-th Element of two sorted arrays
+
 int nums1[] = {1,4,7,10,12};
     int nums2[] = {2,3,6,15};
-    int m = sizeof(nums1)/sizeof(nums1[0]);
-    int n = sizeof(nums2)/sizeof(nums2[0]);
-    cout<<"The Median of two sorted arrays is "<<fixed<<setprecision(5)
-    <<median(nums1,nums2,m,n);
-
+    int n = sizeof(nums1)/sizeof(nums1[0]);
+    int m = sizeof(nums2)/sizeof(nums2[0]);
+    int k;
+    cin>>k;
+    int ans=kthElement(nums1,nums2,m,n,k);
+    cout<<"K-th Element of two sorted arrays : "<<ans<<" ";
 
 
 
