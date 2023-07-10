553f21982e5a5a8686d3da8a92d60f469ec89f4a
Ravi Kumar Gupt <87171097+itsforravi@users.noreply.github.com>
2023-07-10 23:42:34 +0530

    dsa

--- BASICS/13.Binary Trees [Traversals, Medium and Hard Problems]/1.Traversals/Traversal.cpp
+++ BASICS/13.Binary Trees [Traversals, Medium and Hard Problems]/1.Traversals/Traversal.cpp
@@ -199,12 +199,12 @@ int main(){
     //     cout<<postorder[i]<<" ";
     // }
 
-
+// // Level Order Traversal of a Binary Tree
  struct Node * root = newNode(1);
     root->left = newNode(2);
     root->right = newNode(3);
     root->left->left = newNode(4);
-    root->left->right = newNode(5);
+    root->left->right = newNode(6);
  
     cout << "Level Order traversal of binary tree is \n";
     printLevelOrder(root);
