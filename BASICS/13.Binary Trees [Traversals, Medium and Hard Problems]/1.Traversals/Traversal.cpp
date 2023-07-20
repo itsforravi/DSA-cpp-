48ac8d565ecd9e8823dd001df733110a6e93ab1f
Ravi Kumar Gupt <87171097+itsforravi@users.noreply.github.com>
2023-07-20 21:46:54 +0530

    dsa

--- BASICS/13.Binary Trees [Traversals, Medium and Hard Problems]/1.Traversals/Traversal.cpp
+++ BASICS/13.Binary Trees [Traversals, Medium and Hard Problems]/1.Traversals/Traversal.cpp
@@ -414,11 +414,11 @@ int maxDepth(node* node)
     if (node == NULL)
         return 0;
     else {
-        /* compute the depth of each subtree */
+        
         int lDepth = maxDepth(node->left);
         int rDepth = maxDepth(node->right);
  
-        /* use the larger one */
+        
         if (lDepth > rDepth)
             return (lDepth + 1);
         else
