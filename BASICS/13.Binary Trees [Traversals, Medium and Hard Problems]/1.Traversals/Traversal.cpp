cd3c8ad7ec49c87ffc732ff08396e2fb2d5fcdd2
Ravi Kumar Gupt <87171097+itsforravi@users.noreply.github.com>
2023-07-20 21:46:16 +0530

    dsa

--- BASICS/13.Binary Trees [Traversals, Medium and Hard Problems]/1.Traversals/Traversal.cpp
+++ BASICS/13.Binary Trees [Traversals, Medium and Hard Problems]/1.Traversals/Traversal.cpp
@@ -331,54 +331,104 @@ using namespace std;
 
 // Preorder Inorder Postorder Traversals in One Traversal
 
+// struct node{
+//   int data;
+//   struct node *left,*right;
+// };
+
+// void allTraversal(node * root,vector<int> &pre,vector<int> &in,vector<int> &post)
+// {
+//   stack<pair<node *,int>> st;
+//   st.push({
+//     root,1
+//   });
+//   if(root==NULL) return;
+//   while (!st.empty())
+//   {
+//     auto it=st.top();
+//     st.pop();
+
+//     if(it.second==1){
+//       pre.push_back(it.first->data);
+//       it.second++;
+//       st.push(it);
+//       if(it.first->left!=NULL){
+//         st.push({
+//           it.first->left,
+//           1
+//         });
+
+//       }
+
+
+
+//     }
+// else if(it.second==2){
+//       in.push_back(it.first->data);
+//       it.second++;
+//       st.push(it);
+//       if(it.first->right!=NULL){
+//         st.push({
+//           it.first->right,1
+//         });
+
+//       }}
+//       else{
+//         post.push_back(it.first->data);
+//       }}
+  
+// }
+// struct node * newNode(int data) {
+//   struct node * node = (struct node * ) malloc(sizeof(struct node));
+//   node -> data = data;
+//   node -> left = NULL;
+//   node -> right = NULL;
+
+//   return (node);
+// }
+
+
+// Height of tree in binary search
+
 struct node{
   int data;
   struct node *left,*right;
 };
+// vector<int> heightoftree(node * curr){
+//   if(curr==NULL) return heightoftree ;
+//   else{
+//     node * ldepth=heightoftree(curr->left);
+//     node * rdepth=heightoftree(curr->right);
 
-void allTraversal(node * root,vector<int> &pre,vector<int> &in,vector<int> &post)
+//     if(ldepth>rdepth){
+//       return(ldepth+1);
+//     }
+//     else{
+//       return(rdepth+1);
+//     }
+//   }
+// }
+
+int maxDepth(node* node)
 {
-  stack<pair<node *,int>> st;
-  st.push({
-    root,1
-  });
-  if(root==NULL) return;
-  while (!st.empty())
-  {
-    auto it=st.top();
-    st.pop();
-
-    if(it.second==1){
-      pre.push_back(it.first->data);
-      it.second++;
-      st.push(it);
-      if(it.first->left!=NULL){
-        st.push({
-          it.first->left,
-          1
-        });
-
-      }
-
-
-
+    if (node == NULL)
+        return 0;
+    else {
+        /* compute the depth of each subtree */
+        int lDepth = maxDepth(node->left);
+        int rDepth = maxDepth(node->right);
+ 
+        /* use the larger one */
+        if (lDepth > rDepth)
+            return (lDepth + 1);
+        else
+            return (rDepth + 1);
     }
-else if(it.second==2){
-      in.push_back(it.first->data);
-      it.second++;
-      st.push(it);
-      if(it.first->right!=NULL){
-        st.push({
-          it.first->right,1
-        });
-
-      }}
-      else{
-        post.push_back(it.first->data);
-      }}
-  
 }
-struct node * newNode(int data) {
+
+
+
+ struct node * newNode(int data) {
   struct node * node = (struct node * ) malloc(sizeof(struct node));
   node -> data = data;
   node -> left = NULL;
@@ -395,9 +445,6 @@ struct node * newNode(int data) {
 
 
 
-
-
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -405,15 +452,15 @@ int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
-    // struct node * root=newNode(1);
-    // root->left=newNode(2);
-    // root->left->left=newNode(4);
-    // root->left->right=newNode(15);
-    // // root->left->right->left=newNode(6);
-    // root->right=newNode(3);
-    // root->right->left=newNode(6);
-    // root->right->left->right=newNode(7);
-    // root->right->left->right->right=newNode(8);
+    struct node * root=newNode(1);
+    root->left=newNode(2);
+    root->left->left=newNode(4);
+    root->left->right=newNode(15);
+    // root->left->right->left=newNode(6);
+    root->right=newNode(3);
+    root->right->left=newNode(6);
+    root->right->left->right=newNode(7);
+    root->right->left->right->right=newNode(8);
     // root->right->right->left=newNode(9);
     // root->right->right->right=newNode(10);
 
@@ -490,35 +537,37 @@ int main(){
     //     cout<<postOrder[i]<<" ";
     // }
 
-     struct node * root = newNode(1);
-  root -> left = newNode(2);
-  root -> left -> left = newNode(4);
-  root -> left -> right = newNode(5);
-  root -> right = newNode(3);
-  root -> right -> left = newNode(6);
-  root -> right -> right = newNode(7);
+  //    struct node * root = newNode(1);
+  // root -> left = newNode(2);
+  // root -> left -> left = newNode(4);
+  // root -> left -> right = newNode(5);
+  // root -> right = newNode(3);
+  // root -> right -> left = newNode(6);
+  // root -> right -> right = newNode(7);
 
-  vector < int > pre, in , post;
-  allTraversal(root, pre, in , post);
+  // vector < int > pre, in , post;
+  // allTraversal(root, pre, in , post);
 
-  cout << "The preorder Traversal is : ";
-  for (auto nodeVal: pre) {
-    cout << nodeVal << " ";
-  }
-  cout << endl;
-  cout << "The inorder Traversal is : ";
-  for (auto nodeVal: in ) {
-    cout << nodeVal << " ";
-  }
-  cout << endl;
-  cout << "The postorder Traversal is : ";
-  for (auto nodeVal: post) {
-    cout << nodeVal << " ";
-  }
-  cout << endl;
+  // cout << "The preorder Traversal is : ";
+  // for (auto nodeVal: pre) {
+  //   cout << nodeVal << " ";
+  // }
+  // cout << endl;
+  // cout << "The inorder Traversal is : ";
+  // for (auto nodeVal: in ) {
+  //   cout << nodeVal << " ";
+  // }
+  // cout << endl;
+  // cout << "The postorder Traversal is : ";
+  // for (auto nodeVal: post) {
+  //   cout << nodeVal << " ";
+  // }
+  // cout << endl;
 
 
 
+// Height of tree in binary search
+cout << "Height of tree is " << maxDepth(root);
 
 
 
