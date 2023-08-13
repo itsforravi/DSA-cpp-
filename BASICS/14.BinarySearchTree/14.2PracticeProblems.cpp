0caafb8b2c2b9c69c063356e220dcfeb70ed7982
itsforravi <itsforravi@gmail.com>
2023-08-13 10:07:09 +0530

    dsa

--- BASICS/14.BinarySearchTree/14.2PracticeProblems.cpp
+++ BASICS/14.BinarySearchTree/14.2PracticeProblems.cpp
@@ -255,7 +255,6 @@ struct Node {
     struct Node *left, *right;
 };
  
-// A utility function to create a new BST node
 Node* newNode(int item)
 {
     Node* temp = new Node;
@@ -264,7 +263,6 @@ Node* newNode(int item)
     return temp;
 }
  
-// A utility function to do inorder traversal of BST
 void inorder(Node* root)
 {
     if (root != NULL) {
@@ -273,35 +271,25 @@ void inorder(Node* root)
         inorder(root->right);
     }
 }
- 
-/* A utility function to insert a new node with given key in
- * BST */
+
 Node* insert(Node* node, int key)
 {
-    /* If the tree is empty, return a new node */
     if (node == NULL)
         return newNode(key);
  
-    /* Otherwise, recur down the tree */
     if (key < node->key)
         node->left = insert(node->left, key);
     else
         node->right = insert(node->right, key);
  
-    /* return the (unchanged) node pointer */
     return node;
 }
  
-/* Given a binary search tree and a key, this function
-   deletes the key and returns the new root */
 Node* deleteNode(Node* root, int k)
 {
-    // Base case
     if (root == NULL)
         return root;
  
-    // Recursive calls for ancestors of
-    // node to be deleted
     if (root->key > k) {
         root->left = deleteNode(root->left, k);
         return root;
@@ -311,10 +299,7 @@ Node* deleteNode(Node* root, int k)
         return root;
     }
  
-    // We reach here when root is the node
-    // to be deleted.
- 
-    // If one of the children is empty
+   
     if (root->left == NULL) {
         Node* temp = root->right;
         delete root;
@@ -326,33 +311,24 @@ Node* deleteNode(Node* root, int k)
         return temp;
     }
  
-    // If both children exist
     else {
  
         Node* succParent = root;
  
-        // Find successor
         Node* succ = root->right;
         while (succ->left != NULL) {
             succParent = succ;
             succ = succ->left;
         }
  
-        // Delete successor.  Since successor
-        // is always left child of its parent
-        // we can safely make successor's right
-        // right child as left of its parent.
-        // If there is no succ, then assign
-        // succ->right to succParent->right
+   
         if (succParent != root)
             succParent->left = succ->right;
         else
             succParent->right = succ->right;
  
-        // Copy Successor Data to root
         root->key = succ->key;
  
-        // Delete Successor and return root
         delete succ;
         return root;
     }
