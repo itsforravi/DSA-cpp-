89270d80ba1425c2f12ab31176d75b3134746c53
itsforravi <itsforravi@gmail.com>
2023-08-14 22:34:08 +0530

    dsa

--- BASICS/14.BinarySearchTree/14.2PracticeProblems.cpp
+++ BASICS/14.BinarySearchTree/14.2PracticeProblems.cpp
@@ -623,57 +623,165 @@ using namespace std;
 
 // Binary Search Tree Iterator(merge 2 BST)
 
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
-};
+// struct Node {
+//     int data;
+//     Node* left;
+//     Node* right;
+// };
  
-Node* newNode(int data)
-{
-    Node* node = new Node;
-    node->data = data;
-    node->left = node->right = NULL;
-    return node;
-}
+// Node* newNode(int data)
+// {
+//     Node* node = new Node;
+//     node->data = data;
+//     node->left = node->right = NULL;
+//     return node;
+// }
 
-class InorderIterator {
-private:
-    stack<Node*> traversal;
+// class InorderIterator {
+// private:
+//     stack<Node*> traversal;
  
+// public:
+//     InorderIterator(Node* root)
+//     {
+//         moveLeft(root);
+//     }
+ 
+//     void moveLeft(Node* current)
+//     {
+//         while (current) {
+//             traversal.push(current);
+//             current = current->left;
+//         }
+//     }
+ 
+//     bool hasNext()
+//     {
+//         return !traversal.empty();
+//     }
+ 
+//     Node* next()
+//     {
+//         if (!hasNext())
+//             throw "No such element Exists";
+ 
+//         Node* current = traversal.top();
+//         traversal.pop();
+ 
+//         if (current->right)
+//             moveLeft(current->right);
+ 
+//         return current;
+//     }
+// };
+
+
+// Two Sum In BST | Check if there exists a pair with Sum K
+#define MAX_SIZE 100
+class node {
 public:
-    InorderIterator(Node* root)
-    {
-        moveLeft(root);
-    }
- 
-    void moveLeft(Node* current)
-    {
-        while (current) {
-            traversal.push(current);
-            current = current->left;
-        }
-    }
- 
-    bool hasNext()
-    {
-        return !traversal.empty();
-    }
- 
-    Node* next()
-    {
-        if (!hasNext())
-            throw "No such element Exists";
- 
-        Node* current = traversal.top();
-        traversal.pop();
- 
-        if (current->right)
-            moveLeft(current->right);
- 
-        return current;
-    }
+    int val;
+    node *left, *right;
 };
+ 
+// Stack type
+class Stack {
+public:
+    int size;
+    int top;
+    node** array;
+};
+Stack* createStack(int size)
+{
+    Stack* stack = new Stack();
+    stack->size = size;
+    stack->top = -1;
+    stack->array = new node*[(stack->size * sizeof(node*))];
+    return stack;
+}
+int isFull(Stack* stack)
+{
+    return stack->top - 1 == stack->size;
+}
+ 
+int isEmpty(Stack* stack)
+{
+    return stack->top == -1;
+}
+ 
+void push(Stack* stack, node* node)
+{
+    if (isFull(stack))
+        return;
+    stack->array[++stack->top] = node;
+}
+ 
+node* pop(Stack* stack)
+{
+    if (isEmpty(stack))
+        return NULL;
+    return stack->array[stack->top--];
+}
+bool isPairPresent(node* root, int target)
+{
+      Stack* s1 = createStack(MAX_SIZE);
+    Stack* s2 = createStack(MAX_SIZE);
+    bool done1 = false, done2 = false;
+    int val1 = 0, val2 = 0;
+    node *curr1 = root, *curr2 = root;
+    while (1) {
+         while (done1 == false) {
+            if (curr1 != NULL) {
+                push(s1, curr1);
+                curr1 = curr1->left;
+            }
+            else {
+                if (isEmpty(s1))
+                    done1 = 1;
+                else {
+                    curr1 = pop(s1);
+                    val1 = curr1->val;
+                    curr1 = curr1->right;
+                    done1 = 1;
+                }
+            }
+        }
+         while (done2 == false) {
+            if (curr2 != NULL) {
+                push(s2, curr2);
+                curr2 = curr2->right;
+            }
+            else {
+                if (isEmpty(s2))
+                    done2 = 1;
+                else {
+                    curr2 = pop(s2);
+                    val2 = curr2->val;
+                    curr2 = curr2->left;
+                    done2 = 1;
+                }
+            }
+        }
+         if ((val1 != val2) && (val1 + val2) == target) {
+            cout << "Pair Found: " << val1 << "+ " << val2 << " = " << target << endl;
+            return true;
+        }
+         else if ((val1 + val2) < target)
+            done1 = false;
+              else if ((val1 + val2) > target)
+            done2 = false;
+  if (val1 >= val2)
+            return false;
+    }
+}
+ 
+node* NewNode(int val)
+{
+    node* tmp = new node();
+    tmp->val = val;
+    tmp->right = tmp->left = NULL;
+    return tmp;
+}
 
 
 int main(){
@@ -818,6 +926,8 @@ int main(){
 
 
 //  Construct a BST from a preorder traversal
+
+
 //  vector<int> nodeData = { 10, 5, 1, 7, 40, 50 };
  
 //  cout<<"BST from a preorder traversal & TC->O(nlogn)"<<endl;
@@ -839,6 +949,8 @@ int main(){
 
 
 // Inorder Successor/Predecessor in BST
+
+
 //  int key=55;
 //    node *root = NULL;
 //     root = insert(root, 50);
@@ -862,27 +974,43 @@ int main(){
 
 
     // Binary Search Tree Iterator(merge 2 BST)
-   Node* root = newNode(8);
-    root->right = newNode(9);
-    root->left = newNode(3);
-    root->left->left = newNode(2);
-    root->left->right = newNode(4);
-    root->left->right->right = newNode(5);
-     cout<<"Binary Search Tree Iterator(merge 2 BST): ";
-    InorderIterator itr(root);
-    try {
-        cout << itr.next()->data << " ";
-        cout << itr.hasNext() << " ";
-        cout << itr.next()->data << " ";
-        cout << itr.next()->data << " ";
-        cout << itr.next()->data << " ";
-        cout << itr.hasNext() << " ";
-        cout << itr.next()->data << " ";
-        cout << itr.next()->data << " ";
-        cout << itr.hasNext() << " ";
-    }
-    catch (const char* msg) {
-        cout << msg;
-    }
+//    Node* root = newNode(8);
+//     root->right = newNode(9);
+//     root->left = newNode(3);
+//     root->left->left = newNode(2);
+//     root->left->right = newNode(4);
+//     root->left->right->right = newNode(5);
+//      cout<<"Binary Search Tree Iterator(merge 2 BST): ";
+//     InorderIterator itr(root);
+//     try {
+//         cout << itr.next()->data << " ";
+//         cout << itr.hasNext() << " ";
+//         cout << itr.next()->data << " ";
+//         cout << itr.next()->data << " ";
+//         cout << itr.next()->data << " ";
+//         cout << itr.hasNext() << " ";
+//         cout << itr.next()->data << " ";
+//         cout << itr.next()->data << " ";
+//         cout << itr.hasNext() << " ";
+//     }
+//     catch (const char* msg) {
+//         cout << msg;
+//     }
+
+// Two Sum In BST | Check if there exists a pair with Sum K
+
+   node* root = NewNode(15);
+    root->left = NewNode(10);
+    root->right = NewNode(20);
+    root->left->left = NewNode(8);
+    root->left->right = NewNode(12);
+    root->right->left = NewNode(16);
+    root->right->right = NewNode(25);
+ 
+    int target = 23;
+    if (isPairPresent(root, target) == false)
+        cout << "\nNo such values are found\n";
+
+
     return 0;
 }
