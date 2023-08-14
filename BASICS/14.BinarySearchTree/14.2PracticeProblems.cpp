9d2031a3244deac1dcb6c1172744d1bbf3b4e364
itsforravi <itsforravi@gmail.com>
2023-08-14 16:24:20 +0530

    dsa

--- BASICS/14.BinarySearchTree/14.2PracticeProblems.cpp
+++ BASICS/14.BinarySearchTree/14.2PracticeProblems.cpp
@@ -498,72 +498,125 @@ using namespace std;
 
 
 //  Other Methods (The trick is to set a range {min .. max} for every node. )
-class node {
-public:
-    int data;
-    node* left;
-    node* right;
-};
+// class node {
+// public:
+//     int data;
+//     node* left;
+//     node* right;
+// };
  
-node* newNode(int data)
-{
-    node* temp = new node();
+// node* newNode(int data)
+// {
+//     node* temp = new node();
  
-    temp->data = data;
-    temp->left = temp->right = NULL;
+//     temp->data = data;
+//     temp->left = temp->right = NULL;
  
-    return temp;
-}
+//     return temp;
+// }
  
 
-node* constructTreeUtil(int pre[], int* preIndex, int key,
-                        int min, int max, int size)
-{
+// node* constructTreeUtil(int pre[], int* preIndex, int key,
+//                         int min, int max, int size)
+// {
     
-    if (*preIndex >= size)
-        return NULL;
+//     if (*preIndex >= size)
+//         return NULL;
  
-    node* root = NULL;
+//     node* root = NULL;
  
    
-    if (key > min && key < max) {
+//     if (key > min && key < max) {
     
-        root = newNode(key);
-        *preIndex = *preIndex + 1;
+//         root = newNode(key);
+//         *preIndex = *preIndex + 1;
  
-        if (*preIndex < size) {
+//         if (*preIndex < size) {
             
-            root->left = constructTreeUtil(pre, preIndex,
-                                           pre[*preIndex],
-                                           min, key, size);
-        }
-        if (*preIndex < size) {
+//             root->left = constructTreeUtil(pre, preIndex,
+//                                            pre[*preIndex],
+//                                            min, key, size);
+//         }
+//         if (*preIndex < size) {
             
-            root->right = constructTreeUtil(pre, preIndex,
-                                            pre[*preIndex],
-                                            key, max, size);
+//             root->right = constructTreeUtil(pre, preIndex,
+//                                             pre[*preIndex],
+//                                             key, max, size);
+//         }
+//     }
+ 
+//     return root;
+// }
+ 
+// node* constructTree(int pre[], int size)
+// {
+//     int preIndex = 0;
+//     return constructTreeUtil(pre, &preIndex, pre[0],
+//                              INT_MIN, INT_MAX, size);
+// }
+// void printInorder(node* node)
+// {
+//     if (node == NULL)
+//         return;
+//     printInorder(node->left);
+//     cout << node->data << " ";
+//     printInorder(node->right);
+// }
+
+
+
+// Inorder Successor/Predecessor in BST
+
+struct node{
+    int key;
+    struct node *left,*right;
+
+};
+void findpresucc(node* root,node* & pre,node * &suc,int key){
+    if(root==NULL) return ;
+    if(root->key==key){
+        if(root->left!=NULL){
+            node * tmp=root->left;
+            while(tmp->right)
+            tmp=tmp->right;
+            pre=tmp;
         }
-    }
- 
-    return root;
-}
- 
-node* constructTree(int pre[], int size)
-{
-    int preIndex = 0;
-    return constructTreeUtil(pre, &preIndex, pre[0],
-                             INT_MIN, INT_MAX, size);
-}
-void printInorder(node* node)
-{
-    if (node == NULL)
+        if(root-> right !=NULL){
+            node *tmp=root->right;
+            while (tmp->left)
+               tmp=tmp->left;
+               suc=tmp;            
+        }
         return;
-    printInorder(node->left);
-    cout << node->data << " ";
-    printInorder(node->right);
+    }
+  if(root->key>key){
+    suc=root;
+    findpresucc(root->left,pre,suc,key);
+  }
+  else{
+    pre=root;
+    findpresucc(root->right,pre,suc,key);
+  }
 }
 
 
+node *newnode(int data){
+    node * temp=new node;
+    temp->key=data;
+    temp->left=temp->right=NULL;
+    return temp;
+}
+
+node *insert(node * node ,int key){
+    if(node==NULL) return newnode(key);
+    if(key<node->key){
+        node ->left=insert(node->left,key);
+    }
+    else{
+        node ->right=insert(node->right,key);
+    }
+    return node;
+}
  
 
 int main(){
@@ -719,14 +772,36 @@ int main(){
 
 //  Other Methods (The trick is to set a range {min .. max} for every node. )
 
- int pre[] = { 10, 5, 1, 7, 40, 50 };
-    int size = sizeof(pre) / sizeof(pre[0]);
-  cout<<"BST from a preorder traversal \n & TC->O(n) : "<<endl;
+//  int pre[] = { 10, 5, 1, 7, 40, 50 };
+//     int size = sizeof(pre) / sizeof(pre[0]);
+//   cout<<"BST from a preorder traversal \n & TC->O(n) : "<<endl;
 
-    node* root = constructTree(pre, size);
+//     node* root = constructTree(pre, size);
  
-    printInorder(root);
- 
+//     printInorder(root);
 
+
+// Inorder Successor/Predecessor in BST
+ int key=55;
+   node *root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    node *pre=NULL,*suc=NULL;
+    findpresucc(root,pre,suc,key);
+    if(pre!=NULL)
+    cout<<"Predecessor is "<<pre->key<<endl;
+    else
+    cout<<"No Predecessor ";
+    if(suc !=NULL)
+    cout<<"Successor is "<<suc->key;
+    else
+    cout<<" No Successor";
+ 
     return 0;
 }
