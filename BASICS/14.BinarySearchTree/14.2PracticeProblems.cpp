20cc37c3594a215de81f23bee3c71671ca9eee91
itsforravi <itsforravi@gmail.com>
2023-08-15 11:17:34 +0530

    dsa

--- BASICS/14.BinarySearchTree/14.2PracticeProblems.cpp
+++ BASICS/14.BinarySearchTree/14.2PracticeProblems.cpp
@@ -677,111 +677,170 @@ using namespace std;
 
 
 // Two Sum In BST | Check if there exists a pair with Sum K
-#define MAX_SIZE 100
-class node {
-public:
-    int val;
-    node *left, *right;
+// #define MAX_SIZE 100
+// class node {
+// public:
+//     int val;
+//     node *left, *right;
+// };
+ 
+// // Stack type
+// class Stack {
+// public:
+//     int size;
+//     int top;
+//     node** array;
+// };
+// Stack* createStack(int size)
+// {
+//     Stack* stack = new Stack();
+//     stack->size = size;
+//     stack->top = -1;
+//     stack->array = new node*[(stack->size * sizeof(node*))];
+//     return stack;
+// }
+// int isFull(Stack* stack)
+// {
+//     return stack->top - 1 == stack->size;
+// }
+ 
+// int isEmpty(Stack* stack)
+// {
+//     return stack->top == -1;
+// }
+ 
+// void push(Stack* stack, node* node)
+// {
+//     if (isFull(stack))
+//         return;
+//     stack->array[++stack->top] = node;
+// }
+ 
+// node* pop(Stack* stack)
+// {
+//     if (isEmpty(stack))
+//         return NULL;
+//     return stack->array[stack->top--];
+// }
+// bool isPairPresent(node* root, int target)
+// {
+//       Stack* s1 = createStack(MAX_SIZE);
+//     Stack* s2 = createStack(MAX_SIZE);
+//     bool done1 = false, done2 = false;
+//     int val1 = 0, val2 = 0;
+//     node *curr1 = root, *curr2 = root;
+//     while (1) {
+//          while (done1 == false) {
+//             if (curr1 != NULL) {
+//                 push(s1, curr1);
+//                 curr1 = curr1->left;
+//             }
+//             else {
+//                 if (isEmpty(s1))
+//                     done1 = 1;
+//                 else {
+//                     curr1 = pop(s1);
+//                     val1 = curr1->val;
+//                     curr1 = curr1->right;
+//                     done1 = 1;
+//                 }
+//             }
+//         }
+//          while (done2 == false) {
+//             if (curr2 != NULL) {
+//                 push(s2, curr2);
+//                 curr2 = curr2->right;
+//             }
+//             else {
+//                 if (isEmpty(s2))
+//                     done2 = 1;
+//                 else {
+//                     curr2 = pop(s2);
+//                     val2 = curr2->val;
+//                     curr2 = curr2->left;
+//                     done2 = 1;
+//                 }
+//             }
+//         }
+//          if ((val1 != val2) && (val1 + val2) == target) {
+//             cout << "Pair Found: " << val1 << "+ " << val2 << " = " << target << endl;
+//             return true;
+//         }
+//          else if ((val1 + val2) < target)
+//             done1 = false;
+//               else if ((val1 + val2) > target)
+//             done2 = false;
+//   if (val1 >= val2)
+//             return false;
+//     }
+// }
+ 
+// node* NewNode(int val)
+// {
+//     node* tmp = new node();
+//     tmp->val = val;
+//     tmp->right = tmp->left = NULL;
+//     return tmp;
+// }
+
+// Recover BST | Correct BST with two nodes swapped
+struct node{
+    int data;
+    struct node *left,*right;
 };
- 
-// Stack type
-class Stack {
-public:
-    int size;
-    int top;
-    node** array;
-};
-Stack* createStack(int size)
-{
-    Stack* stack = new Stack();
-    stack->size = size;
-    stack->top = -1;
-    stack->array = new node*[(stack->size * sizeof(node*))];
-    return stack;
+void swap(int *a,int *b ){
+    int t=*a;
+    *a=*b;
+    *b=t;
 }
-int isFull(Stack* stack)
-{
-    return stack->top - 1 == stack->size;
+
+struct node *newNode(int data){
+    struct node *node=(struct node * )malloc(sizeof(struct node));
+    node->data=data;
+    node->left=NULL;
+    node->right=NULL;
+    return (node);
+   
 }
- 
-int isEmpty(Stack* stack)
-{
-    return stack->top == -1;
+
+void correctBSTUtill(struct node *root,struct node **first,struct node **middle,struct node **last,struct node **prev){
+if(root){
+correctBSTUtill(root->left,first,middle,last,prev);
+if(*prev && root->data < (*prev)->data){
+if(!*first){
+    *first=*prev;
+    *middle=root;
 }
- 
-void push(Stack* stack, node* node)
-{
-    if (isFull(stack))
-        return;
-    stack->array[++stack->top] = node;
+else
+*last=root;
 }
- 
-node* pop(Stack* stack)
-{
-    if (isEmpty(stack))
-        return NULL;
-    return stack->array[stack->top--];
+*prev=root;
+correctBSTUtill(root->right,first,middle,last,prev);
+
 }
-bool isPairPresent(node* root, int target)
-{
-      Stack* s1 = createStack(MAX_SIZE);
-    Stack* s2 = createStack(MAX_SIZE);
-    bool done1 = false, done2 = false;
-    int val1 = 0, val2 = 0;
-    node *curr1 = root, *curr2 = root;
-    while (1) {
-         while (done1 == false) {
-            if (curr1 != NULL) {
-                push(s1, curr1);
-                curr1 = curr1->left;
-            }
-            else {
-                if (isEmpty(s1))
-                    done1 = 1;
-                else {
-                    curr1 = pop(s1);
-                    val1 = curr1->val;
-                    curr1 = curr1->right;
-                    done1 = 1;
-                }
-            }
-        }
-         while (done2 == false) {
-            if (curr2 != NULL) {
-                push(s2, curr2);
-                curr2 = curr2->right;
-            }
-            else {
-                if (isEmpty(s2))
-                    done2 = 1;
-                else {
-                    curr2 = pop(s2);
-                    val2 = curr2->val;
-                    curr2 = curr2->left;
-                    done2 = 1;
-                }
-            }
-        }
-         if ((val1 != val2) && (val1 + val2) == target) {
-            cout << "Pair Found: " << val1 << "+ " << val2 << " = " << target << endl;
-            return true;
-        }
-         else if ((val1 + val2) < target)
-            done1 = false;
-              else if ((val1 + val2) > target)
-            done2 = false;
-  if (val1 >= val2)
-            return false;
-    }
 }
- 
-node* NewNode(int val)
-{
-    node* tmp = new node();
-    tmp->val = val;
-    tmp->right = tmp->left = NULL;
-    return tmp;
+void correctBST(struct node *root){
+    struct node *first,*middle,*last,*prev;
+    first=middle=last=prev=NULL;
+    correctBSTUtill(root,&first,&middle,&last,&prev);
+
+    if(first&& last)
+    swap(&(first->data),&(last->data));
+    else
+     swap(&(first->data),&(middle->data));
+   
 }
+
+
+void printInroder(struct node *node){
+    if(node==NULL)
+    return;
+    printInroder(node->left);
+    cout<<" "<<node->data;
+    printInroder(node->right);
+}
+
+
 
 
 int main(){
@@ -999,18 +1058,31 @@ int main(){
 
 // Two Sum In BST | Check if there exists a pair with Sum K
 
-   node* root = NewNode(15);
-    root->left = NewNode(10);
-    root->right = NewNode(20);
-    root->left->left = NewNode(8);
-    root->left->right = NewNode(12);
-    root->right->left = NewNode(16);
-    root->right->right = NewNode(25);
+//    node* root = NewNode(15);
+//     root->left = NewNode(10);
+//     root->right = NewNode(20);
+//     root->left->left = NewNode(8);
+//     root->left->right = NewNode(12);
+//     root->right->left = NewNode(16);
+//     root->right->right = NewNode(25);
  
-    int target = 23;
-    if (isPairPresent(root, target) == false)
-        cout << "\nNo such values are found\n";
+//     int target = 23;
+//     if (isPairPresent(root, target) == false)
+//         cout << "\nNo such values are found\n";
 
+// Recover BST | Correct BST with two nodes swapped
+struct node *root = newNode(6);
+    root->left        = newNode(10);
+    root->right       = newNode(2);
+    root->left->left  = newNode(1);
+    root->left->right = newNode(3);
+    root->right->right = newNode(12);
+    root->right->left = newNode(7);
+    cout<<"Original BST \n";
+    printInroder(root);
+    correctBST(root);
+    cout<< "\n Inorder travesal of the fixed tree \n";
+    printInroder(root);   
 
     return 0;
 }
