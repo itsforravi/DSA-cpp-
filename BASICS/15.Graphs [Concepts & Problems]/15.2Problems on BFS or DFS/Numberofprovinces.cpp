c119de9baaf21867a28f216133985b4c86785ab3
itsforravi <itsforravi@gmail.com>
2023-08-15 19:42:02 +0530

    dsa

--- BASICS/15.Graphs [Concepts & Problems]/15.2Problems on BFS or DFS/Numberofprovinces.cpp
+++ BASICS/15.Graphs [Concepts & Problems]/15.2Problems on BFS or DFS/Numberofprovinces.cpp
@@ -5,42 +5,88 @@ using namespace std;
 
 // Number of Provinces
 
+// class Solution{
+//     private:
+//     void dfs(int node ,vector<int> adjLs[],int vis[]){
+//      vis[node]=1;
+//      for(auto it: adjLs[node]){
+//      if(!vis[it]){
+//         dfs(it,adjLs,vis);
+//      }
+//      }
+//     }
+// public:
+// int numProvinces(vector<vector<int>>adj,int V ){
+// vector<int>adjLs[V];
+// for(int i=0;i<V;i++){
+//     for(int j=0;j<V;j++){
+//         if(adj[i][j]==1 && i!=j){
+//             adjLs[i].push_back(j);
+//             adjLs[j].push_back(i);
+//         }
+//     }
+// }
+// int vis[V]={0};
+// int cnt=0;
+// for(int i=0;i<V;i++){
+//     if(!vis[i]){
+//         cnt++;
+//         dfs(i,adjLs,vis);
+//     }
+// }
+// return cnt;
+
+// }
+
+// };
+
+
+// Number of connected components in Matrix
 class Solution{
-    private:
-    void dfs(int node ,vector<int> adjLs[],int vis[]){
-     vis[node]=1;
-     for(auto it: adjLs[node]){
-     if(!vis[it]){
-        dfs(it,adjLs,vis);
-     }
-     }
+private:
+void bfs(int row,int col,vector<vector<int>> &vis,vector<vector<char>> &grid){
+    vis[row][col]=1;
+    queue<pair<int,int>>q;
+    q.push({row,col});
+    int n=grid.size();
+    int m=grid[0].size();
+    while(!q.empty()){
+        int row=q.front().first;
+        int col=q.front().second;
+         q.pop();   
+         for(int delrow=-1;delrow<=1;delrow++){
+            for(int delcol=-1;delcol<=1;delcol++){
+                int nrow=row+delrow;
+                int ncol=col+delcol;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
+                 vis[nrow][ncol] =1;
+                 q.push({nrow,ncol});  
+                }
+
+            }
+         } 
+    
     }
+}
+
 public:
-int numProvinces(vector<vector<int>>adj,int V ){
-vector<int>adjLs[V];
-for(int i=0;i<V;i++){
-    for(int j=0;j<V;j++){
-        if(adj[i][j]==1 && i!=j){
-            adjLs[i].push_back(j);
-            adjLs[j].push_back(i);
+int numIslands(vector<vector<char>>&grid){
+    int n=grid.size();
+    int m=grid.size();
+    vector<vector<int>> vis(n,vector<int>(m,0));
+    int cnt=0;
+    for(int row=0;row<n;row++){
+        for(int col;col<m;col){
+            if(!vis[row][col]&& grid[row][col]=='1'){
+                cnt++;
+                bfs(row,col,vis,grid);
+            }
         }
     }
-}
-int vis[V]={0};
-int cnt=0;
-for(int i=0;i<V;i++){
-    if(!vis[i]){
-        cnt++;
-        dfs(i,adjLs,vis);
-    }
-}
-return cnt;
-
+    return cnt;
 }
 
 };
-
-
 
 
 int main(){
@@ -51,14 +97,22 @@ int main(){
     freopen("output.txt","w",stdout);
     #endif
 
-// Number of Provinces
-vector<vector<int>> adj
-{
-    {1,0,1},
-    {0,1,0},
-    {1,0,1}
-};
-Solution obj;
-cout<<"The Nuber of Provinces Is -> "<<obj.numProvinces(adj,3);
-    return 0;
+// // Number of Provinces
+// vector<vector<int>> adj
+// {
+//     {1,0,1},
+//     {0,1,0},
+//     {1,0,1}
+// };
+// Solution obj;
+// cout<<"The Nuber of Provinces Is -> "<<obj.numProvinces(adj,3);
+
+// Number of connected components in Matrix
+//  Solution obj;
+//     cout <<obj.numIslands();
+ 
+
+
+
+   return 0;
 }
