079c1f15c210dae98e2b849693bb1db4dd882345
itsforravi <itsforravi@gmail.com>
2023-08-17 18:39:53 +0530

    dsa

--- BASICS/15.Graphs [Concepts & Problems]/15.2Problems on BFS or DFS/Numberofprovinces.cpp
+++ BASICS/15.Graphs [Concepts & Problems]/15.2Problems on BFS or DFS/Numberofprovinces.cpp
@@ -206,32 +206,85 @@ using namespace std;
 // };
 
 
-// Detect a Cycle in an Undirected Graph using DFS |
-class Solution{
-    private:
-    bool dfs(int node,int parent, int vis[],vector<int> adj[]){
-        vis[node]=1;
-        for(auto adjacentNode:adj[node]){
-            if(!vis[adjacentNode]){
-       if(dfs(adjacentNode,node,vis,adj)==true)
-       return true;
-            }
-            else if(adjacentNode !=parent) return true;
-        }
-        return false;
-    }
-    public:
+// // Detect a Cycle in an Undirected Graph using DFS |
+// class Solution{
+//     private:
+//     bool dfs(int node,int parent, int vis[],vector<int> adj[]){
+//         vis[node]=1;
+//         for(auto adjacentNode:adj[node]){
+//             if(!vis[adjacentNode]){
+//        if(dfs(adjacentNode,node,vis,adj)==true)
+//        return true;
+//             }
+//             else if(adjacentNode !=parent) return true;
+//         }
+//         return false;
+//     }
+//     public:
 
-    bool isCycle(int V ,vector<int> adj[]){
-        int vis[V]={0};
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfs(i,-1,vis,adj)==true) return true;
+//     bool isCycle(int V ,vector<int> adj[]){
+//         int vis[V]={0};
+//         for(int i=0;i<V;i++){
+//             if(!vis[i]){
+//                 if(dfs(i,-1,vis,adj)==true) return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
+
+// Distance of Nearest Cell having 1
+class Solution{
+public:
+vector<vector<int>> nearest(vector<vector<int>> &grid){
+    int n=grid.size();
+    int m=grid[0].size();
+    vector<vector<int>> vis(n,vector<int>(m,0));
+    vector<vector<int>> dis(n,vector<int>(m,0));
+    queue<pair<pair<int,int>,int>>q;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(grid[i][j]==1){
+                q.push({{i,j},0});
+                vis[i][j]=1;
+
+            }
+            else{
+                vis[i][j]=0;
             }
         }
-        return false;
     }
+    int delrow[]={-1,0,+1,0};
+    int delcol[]={0,+1,0,-1};
+
+    while(!q.empty()){
+        int row=q.front().first.first;
+        int col=q.front().first.second;
+        int steps=q.front().second;
+        q.pop();
+        dis[row][col]=steps;
+ for(int i=0;i<4;i++){
+int nrow=row+delrow[i];
+int ncol=col+delrow[i];
+
+if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0){
+    vis[nrow][ncol]=1;
+    q.push({{nrow,ncol},steps+1});
+}
+        }
+
+
+    }
+return dis;
+}
+
+
+
+
 };
+
 
 
 
@@ -297,12 +350,32 @@ int main(){
 //         cout << "0\n";
 
 // Detect a Cycle in an Undirected Graph using DFS |
-vector<int> adj[4]={{2,4},{2},{1,2},{2}};
+// vector<int> adj[4]={{2,4},{2},{1,2},{2}};
+// Solution obj;
+// bool ans=obj.isCycle(4,adj);
+// if(ans)
+// cout<<"1\n";
+// else
+// cout<<"0\n";
+
+
+// Distance of Nearest Cell having 1
+vector<vector<int>> grid{
+
+
+    {0,1,1,0},
+    {1,1,0,0},
+    {0,0,1,1}
+};
 Solution obj;
-bool ans=obj.isCycle(4,adj);
-if(ans)
-cout<<"1\n";
-else
-cout<<"0\n";
+vector<vector<int>> ans=obj.nearest(grid);
+for(auto i: ans){
+    for(auto j :i){
+        cout<<j<<" ";
+        
+    }
+    cout<<" \n ";
+}
+
    return 0;
 }
