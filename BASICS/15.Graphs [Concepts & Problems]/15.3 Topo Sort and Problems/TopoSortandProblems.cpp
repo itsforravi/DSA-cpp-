da0e3788857fde1bc6e452eb9028b4ce5b34bf37
itsforravi <itsforravi@gmail.com>
2023-08-20 12:45:09 +0530

    dsa

--- BASICS/15.Graphs [Concepts & Problems]/15.3 Topo Sort and Problems/TopoSortandProblems.cpp
+++ BASICS/15.Graphs [Concepts & Problems]/15.3 Topo Sort and Problems/TopoSortandProblems.cpp
@@ -68,38 +68,40 @@ using namespace std;
 
 
 // Cycle Detection in Directed Graph using DFS
-class Solution{
-    public:
-    bool isCycle(int V,vector<int> adj[]){
-        int indegree[V]={0};
-        for(int i=0;i<V;i++){
-            for(auto it:adj[i]){
-                indegree[it]++;
-            }
-        }
-        queue<int>q;
-        for(int i=0;i<V;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
-        int cnt=0;
-        while (!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-           cnt++;
-            for(auto it:adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0) q.push(it);
-            }
-        }
-        if(cnt==V) return false;
-        return  true;
+// class Solution{
+//     public:
+//     bool isCycle(int V,vector<int> adj[]){
+//         int indegree[V]={0};
+//         for(int i=0;i<V;i++){
+//             for(auto it:adj[i]){
+//                 indegree[it]++;
+//             }
+//         }
+//         queue<int>q;
+//         for(int i=0;i<V;i++){
+//             if(indegree[i]==0){
+//                 q.push(i);
+//             }
+//         }
+//         int cnt=0;
+//         while (!q.empty())
+//         {
+//             int node=q.front();
+//             q.pop();
+//            cnt++;
+//             for(auto it:adj[node]){
+//                 indegree[it]--;
+//                 if(indegree[it]==0) q.push(it);
+//             }
+//         }
+//         if(cnt==V) return false;
+//         return  true;
         
-    }
-};
+//     }
+// };
 
+
+// Course Schedule I and II
 
 
 
@@ -140,18 +142,22 @@ int main(){
 
 
 // Cycle Detection in Directed Graph using DFS
-vector<int> adj[5]={{2},{3},{34,5},{2},{}};
-int V=5;
-Solution obj;
-bool ans=obj.isCycle(V,adj);
-cout<<" Cycle Detection in Directed Graph using DFS->"<<endl;
-if(ans){
-    cout<<"True\n";
-}
-else{
-    cout<<"False\n";
-}
-cout<<endl;
+// vector<int> adj[5]={{2},{3},{34,5},{2},{}};
+// int V=5;
+// Solution obj;
+// bool ans=obj.isCycle(V,adj);
+// cout<<" Cycle Detection in Directed Graph using DFS->"<<endl;
+// if(ans){
+//     cout<<"True\n";
+// }
+// else{
+//     cout<<"False\n";
+// }
+// cout<<endl;
+
+// Course Schedule I and II
+
+
 
 return 0;
 }
