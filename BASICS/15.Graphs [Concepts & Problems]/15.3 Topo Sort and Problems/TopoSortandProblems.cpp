3b697b3ea8bf4cc6eb31f1fda2fe86da9ef6f01b
itsforravi <itsforravi@gmail.com>
2023-08-20 17:31:40 +0530

    dsa

--- BASICS/15.Graphs [Concepts & Problems]/15.3 Topo Sort and Problems/TopoSortandProblems.cpp
+++ BASICS/15.Graphs [Concepts & Problems]/15.3 Topo Sort and Problems/TopoSortandProblems.cpp
@@ -218,7 +218,64 @@ class Solution{
     }
 };
 
+class solution{
+    private:
+    vector<int> topoSort(int V, vector<int> adj[])
+	{
+		int indegree[V] = {0};
+		for (int i = 0; i < V; i++) {
+			for (auto it : adj[i]) {
+				indegree[it]++;
+			}
+		}
 
+		queue<int> q;
+		for (int i = 0; i < V; i++) {
+			if (indegree[i] == 0) {
+				q.push(i);
+			}
+		}
+		vector<int> topo;
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			topo.push_back(node);
+			// node is in your topo sort
+			// so please remove it from the indegree
+
+			for (auto it : adj[node]) {
+				indegree[it]--;
+				if (indegree[it] == 0) q.push(it);
+			}
+		}
+
+		return topo;
+	}
+
+
+    public:
+    string findAlienDict(string dict[],int N,int k){
+        vector<int> adj[k];
+        for(int i=0;i<N-1;i++){
+            string s1=dict[i];
+            string s2=dict[i+1];
+            int len=min(s1.size(),s2.size());
+            for(int prt=0;prt<len;prt++){
+                if(s1[prt]!=s2[prt]){
+                    adj[s1[prt]-'a'].push_back(s2[prt]-'a');
+                  break;    
+            
+                }
+            }
+        }
+        vector<int> topo=topoSort(k,adj);
+        string ans=" ";
+        for(auto it:topo){
+            ans=ans+char(it+'a');
+        }
+        return ans;
+    }
+};
 
 
 int main(){
@@ -307,15 +364,27 @@ int main(){
 
 // Find Eventual Safe States – BFS – Topological Sort
 
-vector<int> adj[12]={{1}, {2}, {3, 4}, {4, 5}, {6}, {6}, {7}, {}, {1, 9}, {10},
-		{8}, {9}};
-        int V=12;
-        Solution obj;
-       vector<int> safeNodes = obj.eventualSafeNodes(V, adj);
+// vector<int> adj[12]={{1}, {2}, {3, 4}, {4, 5}, {6}, {6}, {7}, {}, {1, 9}, {10},
+// 		{8}, {9}};
+//         int V=12;
+//         Solution obj;
+//        vector<int> safeNodes = obj.eventualSafeNodes(V, adj);
 
-	for (auto node : safeNodes) {
-		cout << node << " ";
-	}
-	cout << endl; 
+// 	for (auto node : safeNodes) {
+// 		cout << node << " ";
+// 	}
+// 	cout << endl; 
+
+int N=5,k=4;
+string dict[]={"baa","abcd","abca","cab","cad"};
+solution obj;
+string ans=obj.findAlienDict(dict,N,k);
+for(auto ch:ans){
+    cout<<ch<<" ";
+  
+}
+
+
+
 return 0;
 }
