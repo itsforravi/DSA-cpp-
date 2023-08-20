b31f4a9ee31cd9f459c241cf52e42dbec583ddd5
itsforravi <itsforravi@gmail.com>
2023-08-20 10:47:32 +0530

    dsa

--- BASICS/15.Graphs [Concepts & Problems]/15.3 Topo Sort and Problems/TopoSortandProblems.cpp
+++ BASICS/15.Graphs [Concepts & Problems]/15.3 Topo Sort and Problems/TopoSortandProblems.cpp
@@ -3,6 +3,36 @@ typedef unsigned long long int ll;
 using namespace std;
 
 // Topological Sort Algorithm 
+class Solution {
+private:
+	void dfs(int node, int vis[], stack<int> &st,
+	         vector<int> adj[]) {
+		vis[node] = 1;
+		for (auto it : adj[node]) {
+			if (!vis[it]) dfs(it, vis, st, adj);
+		}
+		st.push(node);
+	}
+public:
+	 
+	vector<int> topoSort(int V, vector<int> adj[])
+	{
+		int vis[V] = {0};
+		stack<int> st;
+		for (int i = 0; i < V; i++) {
+			if (!vis[i]) {
+				dfs(i, vis, st, adj);
+			}
+		}
+
+		vector<int> ans;
+		while (!st.empty()) {
+			ans.push_back(st.top());
+			st.pop();
+		}
+		return ans;
+	}
+};
 
 
 
@@ -20,9 +50,17 @@ int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
-    
-// Topological Sort Algorithm 
 
+// Topological Sort Algorithm 
+vector<int> adj[6] = {{}, {}, {3}, {1}, {0, 1}, {0, 2}};
+	int V = 6;
+	Solution obj;
+	vector<int> ans = obj.topoSort(V, adj);
+   cout<<"Topological Order of the Directed Acyclic Graph->"<<endl;
+	for (auto node : ans) {
+		cout << node << " ";
+	}
+	cout << endl;
 
 return 0;
 }
