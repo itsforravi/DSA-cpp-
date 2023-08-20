ad3e1c6fed1120d04add8c600bd7553030fa3052
itsforravi <itsforravi@gmail.com>
2023-08-20 16:58:27 +0530

    dsa

--- BASICS/15.Graphs [Concepts & Problems]/15.3 Topo Sort and Problems/TopoSortandProblems.cpp
+++ BASICS/15.Graphs [Concepts & Problems]/15.3 Topo Sort and Problems/TopoSortandProblems.cpp
@@ -102,49 +102,49 @@ using namespace std;
 
 
 // Course Schedule I 
-class Solution{
-public:
-bool isPossible(int V, vector<pair<int, int> >& prerequisites) {
-		vector<int> adj[V];
-		for (auto it : prerequisites) {
-			adj[it.first].push_back(it.second);
-		}
+// class Solution{
+// public:
+// bool isPossible(int V, vector<pair<int, int> >& prerequisites) {
+// 		vector<int> adj[V];
+// 		for (auto it : prerequisites) {
+// 			adj[it.first].push_back(it.second);
+// 		}
 
 
 
-		int indegree[V] = {0};
-		for (int i = 0; i < V; i++) {
-			for (auto it : adj[i]) {
-				indegree[it]++;
-			}
-		}
+// 		int indegree[V] = {0};
+// 		for (int i = 0; i < V; i++) {
+// 			for (auto it : adj[i]) {
+// 				indegree[it]++;
+// 			}
+// 		}
 
-		queue<int> q;
-		for (int i = 0; i < V; i++) {
-			if (indegree[i] == 0) {
-				q.push(i);
-			}
-		}
-		vector<int> topo;
-		while (!q.empty()) {
-			int node = q.front();
-			q.pop();
-			topo.push_back(node);
-			// node is in your topo sort
-			// so please remove it from the indegree
+// 		queue<int> q;
+// 		for (int i = 0; i < V; i++) {
+// 			if (indegree[i] == 0) {
+// 				q.push(i);
+// 			}
+// 		}
+// 		vector<int> topo;
+// 		while (!q.empty()) {
+// 			int node = q.front();
+// 			q.pop();
+// 			topo.push_back(node);
+// 			// node is in your topo sort
+// 			// so please remove it from the indegree
 
-			for (auto it : adj[node]) {
-				indegree[it]--;
-				if (indegree[it] == 0) q.push(it);
-			}
-		}
+// 			for (auto it : adj[node]) {
+// 				indegree[it]--;
+// 				if (indegree[it] == 0) q.push(it);
+// 			}
+// 		}
 
-		if (topo.size() == V) return true;
-		return false;
+// 		if (topo.size() == V) return true;
+// 		return false;
 
 
-	}
-};
+// 	}
+// };
 
 
 // Course Schedule II
@@ -185,6 +185,38 @@ bool isPossible(int V, vector<pair<int, int> >& prerequisites) {
 // };
 
 
+// Find Eventual Safe States – BFS – Topological Sort
+class Solution{
+    public:
+    vector<int>eventualSafeNodes(int V ,vector<int>adj[]){
+        vector<int>adjRev[V];
+        int indegree[V]={0};
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                adjRev[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        queue<int>q;
+        vector<int>safenode;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int node =q.front();
+            q.pop();
+            safenode.push_back(node);
+            for(auto it:adjRev[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        sort(safenode.begin(),safenode.end());
+        return safenode;
+    }
+};
 
 
 
@@ -237,18 +269,18 @@ int main(){
 // cout<<endl;
 
 /// Course Schedule I 
-vector<pair<int, int>> prerequisites;
-	int N = 4;
-	prerequisites.push_back({1, 0});
-	prerequisites.push_back({2, 1});
-	prerequisites.push_back({3, 2});
+// vector<pair<int, int>> prerequisites;
+// 	int N = 4;
+// 	prerequisites.push_back({1, 0});
+// 	prerequisites.push_back({2, 1});
+// 	prerequisites.push_back({3, 2});
 
-	Solution obj;
-	bool ans = obj.isPossible(N, prerequisites);
+// 	Solution obj;
+// 	bool ans = obj.isPossible(N, prerequisites);
 
-	if (ans) cout << "YES";
-	else cout << "NO";
-	cout << endl;
+// 	if (ans) cout << "YES";
+// 	else cout << "NO";
+// 	cout << endl;
 
 // Course Schedule II
 // int N = 4;
@@ -273,6 +305,17 @@ vector<pair<int, int>> prerequisites;
 // 	}
 // 	cout << endl;
 
+// Find Eventual Safe States – BFS – Topological Sort
 
+vector<int> adj[12]={{1}, {2}, {3, 4}, {4, 5}, {6}, {6}, {7}, {}, {1, 9}, {10},
+		{8}, {9}};
+        int V=12;
+        Solution obj;
+       vector<int> safeNodes = obj.eventualSafeNodes(V, adj);
+
+	for (auto node : safeNodes) {
+		cout << node << " ";
+	}
+	cout << endl; 
 return 0;
 }
