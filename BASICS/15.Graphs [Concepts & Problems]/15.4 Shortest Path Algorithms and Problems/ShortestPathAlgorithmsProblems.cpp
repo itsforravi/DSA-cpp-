05cd90d3763d008fa99986320b4fbcebe818e415
itsforravi <itsforravi@gmail.com>
2023-08-25 21:26:06 +0530

    dsa

--- BASICS/15.Graphs [Concepts & Problems]/15.4 Shortest Path Algorithms and Problems/ShortestPathAlgorithmsProblems.cpp
+++ BASICS/15.Graphs [Concepts & Problems]/15.4 Shortest Path Algorithms and Problems/ShortestPathAlgorithmsProblems.cpp
@@ -391,7 +391,7 @@ int main(){
    int V = 3, E = 3, S = 2;
     vector<vector<int>> adj[V];
     vector<vector<int>> edges;
-    vector<int> v1{1, 1}, v2{2, 6}, v3{2, 3}, v4{0, 1}, v5{1, 3}, v6{1, 6};
+    vector<int> v1{1, 1}, v2{2, 6}, v3{2, 3}, v4{0, 1}, v5{1, 3}, v6{0, 6};
     int i = 0;
     adj[0].push_back(v1);
     adj[0].push_back(v2);
